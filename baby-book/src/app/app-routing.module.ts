c0c708f1beb18287af94d65d1556f24010192765
Alden Shiverick <ashiverick@smu.edu>
2018-10-31 17:55:23 -0500

    Working on Routes to Modify-Account Page

--- baby-book/src/app/app-routing.module.ts
+++ baby-book/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ChildrenComponent } from './children/children.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ChildDetailComponent } from './child-detail/child-detail.component';
 import { LoginComponent } from './login/login.component';
+import { ModifyAccountComponent } from './modify-account/modify-account.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: ChildDetailComponent },
   { path: 'children', component: ChildrenComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'modify', component: ModifyAccountComponent}
 ];
 
 @NgModule({
