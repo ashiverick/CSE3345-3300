cc6f74102a33ad282a72cb817b32022996bf30bb
Lagombi <cgoehring@smu.edu>
2018-11-11 13:24:18 -0600

    User service added (needs more routes! POSTs)

---
+++ baby-book/src/app/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  protected endPoint = 'http://ec2-52-15-123-114.us-east-2.compute.amazonaws.com:8080/api/users';
+
+  protected httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(protected httpClient: HttpClient) { }
+
+  getById(id: number): Observable<User> {
+    return this.httpClient
+      .get<User>(`${this.endPoint}/${id}`, this.httpOptions)
+      .pipe(catchError(this.handleException));
+  }
+
+  getAll(): Observable<User []> {
+    return this.httpClient
+      .get<User []>(`${this.endPoint}`, this.httpOptions)
+      .pipe(catchError(this.handleException));
+  }
+
+  protected handleException(exception: any) {
+    // tslint:disable-next-line:no-var-keyword prefer-const
+    var message = `${exception.status} : ${exception.statusText}\r\n${exception.message}`;
+    alert(message);
+    return Observable.throw(exception);
+  }
+}
+
