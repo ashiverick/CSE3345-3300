feea9835166f38ec30852c3538c45ac676fd4c2c
Alden Shiverick <ashiverick@smu.edu>
2018-11-26 17:11:48 -0600

    edits

--- baby-book/src/app/child.ts
+++ baby-book/src/app/child.ts
@@ -2,7 +2,10 @@ import { Post } from './post';
 
 export class Child {
   id?: number;
-  name?: string;
+  firstName?: string;
+  lastName?: string;
+  birthday?: Date;
+  gender?: string;
   profilePicture?: string;
-  posts: Post[];
+  posts?: Post[];
 }
